2f2d3e2c6e810bedcf38265ab653adad50e8b869
jamieberrier <berrier.jamie@gmail.com>
2017-12-11 22:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers){
+  drivers.forEach(function (driver){
+    console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function (drivers, location){
+  drivers.forEach(function (driver){
+    if (driver.hometown===location){
+      console.log(driver.name);
+    }
+  });
+};
+
+const driversByRevenue = function (drivers){
+  let sortedDrivers = drivers.sort();
+  return sortedDrivers;
+}
