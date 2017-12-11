fdb5b5999280857fed7e1095b4c497c170b1e65c
jamieberrier <berrier.jamie@gmail.com>
2017-12-11 23:02:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const driversByRevenue = function (drivers){
 };
 
 const driversByName = function (drivers){
-  drivers.sort(function (){
-    return drivers;
+  return drivers.slice().sort(function (driverOne, driverTwo){
+    return driverOne.name.localeCompare(driverTwo.name);
   });
 };
