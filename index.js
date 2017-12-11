1f57af337558086379e87c40d89c1baf4fc3b9a1
jamieberrier <berrier.jamie@gmail.com>
2017-12-11 23:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const driversByName = function (drivers){
 };
 
 const totalRevenue = function (drivers){
-  return drivers.slice().reduce(function (revenueOne, revenueTwo){
-    return revenueOne + revenueTwo;
+  return drivers.reduce(function (total, currentDriver){
+    return currentDriver.revenue + total;
   }, 0);
 };
